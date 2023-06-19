553d7ffda0e3e2ce1b51012c673c5cf73a591b19
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-06-19 22:57:52 +0900

    add typescript

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,14 +1,40 @@
 import courseData from './courseData'
 
-export function useCourse() {
+interface Lesson {
+  title: string
+  slug: string
+  number: number
+  downloadUrl: string
+  videoId: number
+  text: string
+  sourceUrl?: string
+}
+
+interface Chapter {
+  title: string
+  slug: string
+  number: number
+  lessons: Lesson[]
+}
+
+interface Course {
+  title: string
+  chapters: Chapter[]
+}
+
+export function useCourse(): Course {
+  const chapters: Chapter[] = courseData.chapters.map((chapter) => {
+    const lessons: Lesson[] = chapter.lessons.map((lesson) => ({
+      ...lesson,
+      path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+    }))
+    return {
+      ...chapter,
+      lessons,
+    }
+  })
   return {
     ...courseData,
-    chapters: courseData.chapters.map((chapter) => ({
-      ...chapter,
-      lessons: chapter.lessons.map((lesson) => ({
-        ...lesson,
-        path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
-      })),
-    })),
+    chapters,
   }
 }
