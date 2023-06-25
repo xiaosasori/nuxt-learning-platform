87e54cc72960682d19cc78efc65c081c3f6aed9a
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-06-25 14:32:04 +0900

    fix inline middleware

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -3,36 +3,36 @@ const course = useCourse()
 const route = useRoute()
 
 definePageMeta({
-  middleware: 'auth',
-  // middleware: [
-  //   function ({ params }, _from) {
-  //     const course = useCourse()
-  //     const chapter = course.chapters.find(
-  //       (chapter) => chapter.slug === params.chapterSlug
-  //     )
-  //     if (!chapter) {
-  //       return abortNavigation(
-  //         createError({
-  //           statusCode: 404,
-  //           message: 'Chapter not found',
-  //         })
-  //       )
-  //     }
-  //     const lesson = chapter.lessons.find(
-  //       (lesson) => lesson.slug === params.lessonSlug
-  //     )
-  //     if (!lesson) {
-  //       return abortNavigation(
-  //         createError({
-  //           statusCode: 404,
-  //           message: 'Lesson not found',
-  //         })
-  //       )
-  //     }
-  //     return true
-  //   },
-  //   'auth',
-  // ],
+  middleware: [
+    function ({ params }) {
+      const course = useCourse()
+
+      const chapter = course.chapters.find(
+        (chapter) => chapter.slug === params.chapterSlug
+      )
+      if (!chapter) {
+        return abortNavigation(
+          createError({
+            statusCode: 404,
+            message: 'Chapter not found',
+          })
+        )
+      }
+
+      const lesson = chapter.lessons.find(
+        (lesson) => lesson.slug === params.lessonSlug
+      )
+      if (!lesson) {
+        return abortNavigation(
+          createError({
+            statusCode: 404,
+            message: 'Lesson not found',
+          })
+        )
+      }
+    },
+    'auth',
+  ],
 })
 
 const chapter = computed(() => {
